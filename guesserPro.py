ff4e718dcaa19c75271a457e9a41626bb5f117fd
Gaurav Gali <gauravgali@Gauravs-MacBook-Air.local>
2023-08-15 19:48:40 +0530

    new guesserPro

--- guesserPro.py
+++ guesserPro.py
@@ -1,88 +1,185 @@
+# imports
 from matrix import Matrix
 import random
 
+# guesserPro
 game_data = [
     # {
-    #     "player": ""
-    #     "attempts": 0
+    #     "player": "user/ai",
+    #     "attempts": 0,
+    #     "number": comp_gen,
     # }
 ]
 
-l_limit, u_limit = 0, 100
+# main game function
 
 
-def display_data():
-    print("\n Game Analytics \n")
-    m1 = Matrix("Player", "Attempts")
+def guesserPro():
+    global game_data
+    ai_guesses = []
+    # guess limits
+    l_limit, u_limit = 1, 100
+
+    # Game mode
+    try:
+        mode = int(input(
+            """
+1. For You vs AI
+2. For AI vs AI
+>>> """
+        ))
+    except:
+        errorFormatter("Invalid game mode !")
+        guesserPro()
+
+    # Game Logic
+    game_attempts = 1
+
+    comp_gen = random.randint(l_limit, u_limit)
+    if mode == 1:
+        while True:
+            try:
+                user_gen = int(input("Your Guess : "))
+            except:
+                errorFormatter("Invalid guess !")
+                guesserPro()
+
+            if user_gen > comp_gen:
+                displayFormatter(user_gen, "is greater than the number")
+            elif user_gen < comp_gen:
+                displayFormatter(user_gen, "is lesser to the number")
+            else:
+                game_data.append({
+                    "player": "You",
+                    "attempts": game_attempts,
+                    "number": comp_gen
+                })
+                break
+
+            game_attempts += 1
+
+    elif mode == 2:
+        prev_greater = -1
+        prev_lesser = -1
+        while True:
+
+            if prev_greater is None:
+                l_limit = prev_lesser
+            elif prev_lesser is None:
+                u_limit = prev_greater
+            else:
+                pass
+
+            user_gen = random.randint(l_limit, u_limit)
+
+            if user_gen in ai_guesses:
+                continue
+            else:
+                if user_gen > comp_gen:
+                    ai_guesses.append(user_gen)
+
+                    prev_greater = user_gen
+                    prev_lesser = None
+
+                    displayFormatter(user_gen, "is greater than the number")
+                elif user_gen < comp_gen:
+                    ai_guesses.append(user_gen)
+
+                    prev_lesser = user_gen
+                    prev_greater = None
+
+                    displayFormatter(user_gen, "is lesser to the number")
+                else:
+                    game_data.append({
+                        "player": "AI",
+                        "attempts": game_attempts,
+                        "number": comp_gen
+                    })
+                    break
+                game_attempts += 1
+
+    else:
+        errorFormatter("Invalid game mode !")
+        guesserPro()
+
+    # Game Progression
+    def gameProgression():
+        game_progression_status = input("Do you want to continue? (y/n): ")
+        if game_progression_status == "y":
+            guesserPro()
+        elif game_progression_status == "n":
+            gameAnalytics()
+            return
+        else:
+            errorFormatter("Type either 'y' or 'n' to continue !")
+            gameProgression()
+
+    gameProgression()
+
+# Game Analytics
+
+
+def gameAnalytics():
+    print("\nGame Analytics\n")
+    m1 = Matrix("Player", "Attempts", "Number")
     m1.unlock()
-
-    for data in game_data:
-        m1.add_row(data["player"], data["attempts"])
-
+    for i in game_data:
+        m1.add_row(
+            i["player"],
+            i["attempts"],
+            i["number"],
+        )
     m1.serialize()
+
+    m1.lock()
+
+    m1.printf()
+
+    export_status = input("Do you want to export the analytics? (y/n): ")
+    if export_status == "y":
+        export_type = int(input(
+            """
+1. Export As CSV
+2. Export AS HTML
+>>> """
+        ))
+
+        if export_type == 1:
+            m1.export_csv("./analytics.csv")
+        elif export_type == 2:
+            m1.export_html("./analytics.html")
+        else:
+            pass
+
+    elif export_status == "n":
+        pass
+    else:
+        pass
+
+    print("DONE .")
+
+# display formatter
+
+
+def displayFormatter(guess, message):
+    m1 = Matrix("Guess", "Message")
+    m1.unlock()
+    m1.add_row(guess, message)
+    m1.lock()
+
+    m1.printf()
+
+# error formatter
+
+
+def errorFormatter(message):
+    m1 = Matrix("Status", "Message")
+    m1.unlock()
+    m1.add_row("Error", message)
     m1.lock()
 
     m1.printf()
 
 
-def guesserPro():
-
-    global game_data
-
-    prompt = """
-1. For You vs AI
-2. For AI vs AI
->>> """
-
-    mode = int(input(prompt))
-    cur_rounds = 0
-
-    cgen = random.randint(l_limit, u_limit)
-    while True:
-        if mode == 1:
-            m1 = Matrix("User Number", "Message")
-        else:
-            m1 = Matrix("AI's Number", "Message")
-
-        if mode == 1:
-            ugen = int(input("Your Guess : "))
-        elif mode == 2:
-            ugen = random.randint(l_limit, u_limit)
-
-        if ugen > cgen:
-            m1.unlock()
-            m1.add_row(ugen, "is greater than computer's number")
-            m1.lock()
-            m1.printf()
-        elif ugen < cgen:
-            m1.unlock()
-            m1.add_row(ugen, "is lesser than computer's number")
-            m1.lock()
-            m1.printf()
-        else:
-            break
-
-        cur_rounds += 1
-
-    if mode == 1:
-        player = "You"
-    else:
-        player = "AI"
-
-    game_data.append({
-        "player": player,
-        "attempts": cur_rounds
-    })
-
-    if input("Do you want to continue y/n : ") == "y":
-        guesserPro()
-    else:
-        display_data()
-        return
-
-
-Matrix.pretty_print([
-    ["Guesser Pro"]
-])
-
+# Calling the game function
 guesserPro()
